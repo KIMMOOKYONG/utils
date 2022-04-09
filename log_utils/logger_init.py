6083a6ce9a3abc8f7499f858a2f81fd76bc634fc
KIMMOOKYONG <102650331+KIMMOOKYONG@users.noreply.github.com>
2022-04-09 22:20:10 +0900

    Update logger_init.py

--- log_utils/logger_init.py
+++ log_utils/logger_init.py
@@ -23,6 +23,7 @@ with open(path, 'r') as stream:
 logging.config.dictConfig(logging_config)
 
 # Initialize Log Levels
+# 로깅 레빌 기본값 설정
 log_level = logging.WARNING
 
 # Check For Debug Flag
@@ -33,6 +34,8 @@ log_level = logging.WARNING
 # This level can be overwritten by the following in a file
 #   logger = logging.getlogger(__name__)
 #   logger.setLevel(logging.INFO)
-loggers = [logging.getLogger(name) for name in logging.root.manager.loggerDict]
-for log in loggers:
-  log.setLevel(log_level)
+
+# 모든 로깅에 대해서 단일 모드로 설정하고자 하는 경우 아래 코드를 사용하면 된다.
+# loggers = [logging.getLogger(name) for name in logging.root.manager.loggerDict]
+# for log in loggers:
+#   log.setLevel(log_level)
